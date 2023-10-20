d37b935777397eb9b9c3f0d5634adc1dc76d16f0
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-10-20 12:35:16 -0300

    Puliendo CSS + Iconos Redes

--- only-books-fe/src/Components/Footer/Footer.jsx
+++ only-books-fe/src/Components/Footer/Footer.jsx
@@ -1,8 +1,22 @@
 // import React from 'react'
 import { Link } from 'react-router-dom'
+import { routes } from '../../Utils/routes.js'
 import './Footer.css'
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Footer = () => {
+
+
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+
     return (
         <footer>
             <div className='footer-container'>
@@ -10,30 +24,39 @@ const Footer = () => {
                     <Link>
                         <img style={{
                             width: "150px",
-                        }} src="src\img\logoOnlyBooksv1.jpeg" alt="Logo" />
+                        }} 
+                            src="src\img\logoOnlyBooksv1.jpeg" alt="OnlyBooks_Logo" />
                     </Link>
-                    <div className='social-icons'>
-                        {/* iconos de redes sociales */}
+                    <div className="social-icons">
+                        <Link onClick={handleClick} to={routes.WhatsApp}>
+                            <WhatsAppIcon className="social-icon animation" />
+                        </Link>
+                        <Link onClick={handleClick} to={routes.Instagram}>
+                            <InstagramIcon className="social-icon animation" />
+                        </Link>
+                        <Link onClick={handleClick} to={routes.Facebook}>
+                            <FacebookIcon className="social-icon animation" />
+                        </Link>
+                        <Link onClick={handleClick} to={routes.LinkedIn}>
+                            <LinkedInIcon className="social-icon animation" />
+                        </Link>
                     </div>
-                    <p>2023 OnlyBooks</p>
+                    <p>&copy; {new Date().getFullYear()} OnlyCars - Team 02</p>
                     <p className='copyright'>Todos los derechos reservados</p>
                 </div>
-                {/* <div className='footer-mid'>
-                    <h4>COMPANY</h4>
+                <div className='footer-mid'>
+                    {/* <h4>COMPANY</h4>
                     <p>Projects</p>
-                    <p>Contacts</p>
-                    <p>Open Source</p>
-                    <p>blog</p>
+                    <p>Contacts</p> */}
                 </div>
                 <div className='footer-right'>
-                    <h4>SERVICES</h4>
+                    {/* <h4>SERVICES</h4>
                     <p>Test</p>
-                    <p>Test</p>
-                    <p>Test</p>
-                </div> */}
+                    <p>Test</p> */}
+                </div>
             </div>
         </footer>
     )
 }
 
-export default Footer
+export default Footer;
