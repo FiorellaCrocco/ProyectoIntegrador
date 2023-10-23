a4a57236ed33b2489138295bb78d6b27eaae90f6
martinezrocio <martiroci.98@gmail.com>
2023-10-23 19:57:37 -0300

    subo perfil administrador

--- only-books-fe/src/Routes/Administrador.jsx
+++ only-books-fe/src/Routes/Administrador.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import './Administrador.css'
+import { Link } from 'react-router-dom';
+
 
 const Administrador = () => {
   return (
-    <div>
+    <>
+  <h1 className='bienvenida'>BIENVENIDO AL PANEL DE ADMINISTRACION</h1>
+  <h2 className='subtit'>Que deseas hacer?</h2>
+  <div className='panel'> 
+    <button className='adminBtn'><Link to="/administrador/cargar" className='hipervinc'>Cargar nuevo producto</Link></button>
+    <button className='adminBtn'>Listar productos</button>
+    <button className='adminBtn'>Editar productos</button>
 
+  </div>
       
-    </div>
+    </>
   )
 }
 
