28449746bd8fa9086b31542ff63c90f1fb0fdc24
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-10-25 17:59:10 -0300

    .

--- only-books-fe/src/Routes/Administrador.jsx
+++ only-books-fe/src/Routes/Administrador.jsx
@@ -6,15 +6,15 @@ import { Link } from 'react-router-dom';
 const Administrador = () => {
   return (
     <>
-  <h1 className='bienvenida'>BIENVENIDO AL PANEL DE ADMINISTRACION</h1>
-  <h2 className='subtit'>Que deseas hacer?</h2>
-  <div className='panel'> 
-    <button className='adminBtn'><Link to="/administrador/cargar" className='hipervinc'>Cargar nuevo producto</Link></button>
-    <button className='adminBtn'><Link to="/administrador/listar" className='hipervinc'>Listar productos</Link></button>
-    <button className='adminBtn'>Editar productos</button>
+      <h1 className='bienvenida'>BIENVENIDO AL PANEL DE ADMINISTRACION</h1>
+      <h2 className='subtit'>Que deseas hacer?</h2>
+      <div className='panel'>
+        <Link to="/administrador/cargar" className='hipervinc'> <button className='adminBtn'>Cargar nuevo producto</button> </Link>
+        <Link to="/administrador/listar" className='hipervinc'> <button className='adminBtn'>Listar productos</button> </Link>
+        <button className='adminBtn'>Editar productos</button>
 
-  </div>
-      
+      </div>
+
     </>
   )
 }
