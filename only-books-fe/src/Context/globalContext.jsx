54ccbe9be13d274949617e42971020cc69299038
Fiorella Crocco Grappiolo <fiorellacrocco@gmail.com>
2023-10-26 18:14:57 -0300

    merge recomendados

--- only-books-fe/src/Context/globalContext.jsx
+++ only-books-fe/src/Context/globalContext.jsx
@@ -1,58 +1,34 @@
-/* eslint-disable no-unused-vars */
-import { useState, useEffect, createContext } from "react";
+import React, { useState, useEffect, createContext } from "react";
 
-export const GlobalContext = createContext()
+export const GlobalContext = createContext();
 
-export function BookProvider ({children}){
-    const [listaLibros, setListaLibros] = useState(null)
-    const [isLoading, setIsLoading] = useState(true)
-    const url = "https://onlybooks.isanerd.club/api/book/listar"
-    
-    /*
-    useEffect(()=>{
-        const storedData = localStorage.getItem('bookList')
-        if(storedData){
-            setListaLibros(JSON.parse(storedData))
-            setIsLoading(false)
-        }else{
-            const config ={
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            }
-            fetch(url)
-            .then(response=>{
-                if(!response.ok){
-                    throw new Error('No se encuentra el libro.')
-                }
-            return response.json()
-            })
-            .then(data=>{
-                console.log(data)
-                setListaLibros(data)
-                setIsLoading(false)
-            })
-            .catch(error =>{
-                console.error("Error al cargar los detalles del libro:",error)
-                setIsLoading(false)
-            })
+export const BookProvider = ({ children }) => {
+  const [listaLibros, setListaLibros] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const url = "http://localhost:8080/book/listar";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("No se pudo obtener la lista de libros.");
         }
-    },[])
+        const data = await response.json();
+        setListaLibros(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error al cargar la lista de libros:", error);
+        setIsLoading(false);
+      }
+    };
 
-    if(isLoading){
-        return <div>Cargando...</div>
-    }
-    if(!listaLibros){
-        return <div>No se encontro el libro</div>
-    }*/
+    fetchData();
+  }, []);
 
-    return (
-        <>
-        <GlobalContext.Provider value={listaLibros}>
-            {children}
-        </GlobalContext.Provider>
-        </>
-    )
-
-}
+  return (
+    <GlobalContext.Provider value={{ listaLibros, isLoading }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
