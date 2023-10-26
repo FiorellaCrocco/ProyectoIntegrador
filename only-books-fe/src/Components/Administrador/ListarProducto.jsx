80700408e8e8d811c616ceb9a5d9862d8ab58387
GarciaFernando <garciafc97@gmail.com>
2023-10-26 19:12:45 -0300

    Implementacion Eliminar producto desde front

--- only-books-fe/src/Components/Administrador/ListarProducto.jsx
+++ only-books-fe/src/Components/Administrador/ListarProducto.jsx
@@ -1,54 +1,87 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
-import data from '../LibrosPaginados/libros'
-import "./ListarProducto.css"
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
+import React, { useContext, useEffect, useState } from "react";
+//import data from '../LibrosPaginados/libros'
+import "./ListarProducto.css";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { GlobalContext } from "../../Context/globalContext";
 
 const ListarProducto = () => {
+  const { listaLibros, isLoading } = useContext(GlobalContext);
+  const [productos, setProductos] = useState(listaLibros);
+  console.log(listaLibros)
+  useEffect(()=>{
+    if(!isLoading){
+    setProductos(listaLibros)
+    }
+  },[isLoading,listaLibros])
+  
 
-  const [productos, setProductos] = useState(data);
 
   // IMPLEMENTACION DE REFERENCIA, SOLO BORRA DE MANERA LOCAL, AL REFRESCAR LA PAGINA VUELVE A CARGAR LOS DATOS
   const handleDelete = (id) => {
-    const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar este producto?");
+    const settings = {
+      method:'DELETE',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+    }
+    const url = `http://localhost:8080/book/eliminar/${id}`;
+    const fetchData = async () => {
+      try {
+        const res = await fetch(url,settings);
+        if (!res.ok) {
+          throw new Error("Error al eliminar el libro");
+        }
+        const data = await res.json();
+      } catch (error) {
+        console.log("Error al eliminar el libro", error);
+      }
+    };
+
+    const confirmDelete = window.confirm(
+      "¿Estás seguro de que deseas eliminar este producto?"
+    );
     if (confirmDelete) {
-      const updatedProductos = productos.filter((producto) => producto.id !== id);
+      fetchData();
+      const updatedProductos = productos.filter(
+        (producto) => producto.id !== id
+      );
       setProductos(updatedProductos);
     }
   };
 
-
-
   return (
     <div>
-      <h2 className='titulo'>Listado de productos</h2>
-      <ul className='listaContainer'>
-
+      <h2 className="titulo">Listado de productos</h2>
+      <ul className="listaContainer">
         {productos.map((libro) => (
-          <li className='lista' key={libro.id}>
-            <div className='id'>{libro.id}
-            </div>
-            <div className='nombre'>{libro.title}</div>
-            <div  className='admin-btn-container'>
+          <li className="lista" key={libro.id}>
+            <div className="id">{libro.id}</div>
+            <div className="nombre">{libro.title}</div>
+            <div className="admin-btn-container">
               {/* <button className='btnEdit' onClick={() => handleDelete(libro.id)}>Eliminar</button> */}
-              <Button variant="outlined" color="error" className='btnEdit' onClick={() => handleDelete(libro.id)}>
+              <Button
+                variant="outlined"
+                color="error"
+                className="btnEdit"
+                onClick={() => handleDelete(libro.id)}
+              >
                 Eliminar
               </Button>
               {/* <Button variant="outlined" startIcon={<DeleteIcon />}>
                 Eliminar
               </Button> */}
-              <Button variant="outlined" color="success" className='btnEdit' >
+              <Button variant="outlined" color="success" className="btnEdit">
                 Editar
               </Button>
               {/* <button className='btnEdit'>Editar</button> */}
-
             </div>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ListarProducto
+export default ListarProducto;
