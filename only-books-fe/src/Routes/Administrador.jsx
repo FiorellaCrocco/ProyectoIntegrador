7d32a0067dcade7dfd3a955ccc483d7dde553b30
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-11-01 18:51:41 -0300

    CSS features

--- only-books-fe/src/Routes/Administrador.jsx
+++ only-books-fe/src/Routes/Administrador.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 import './Administrador.css'
 import { Link } from 'react-router-dom';
@@ -5,17 +6,15 @@ import { Link } from 'react-router-dom';
 
 const Administrador = () => {
   return (
-    <>
-      <h1 className='bienvenida'>BIENVENIDO AL PANEL DE ADMINISTRACION</h1>
+    <div className='admin-panel'>
+      <h1 className='bienvenida'>Panel de Administracion</h1>
       <h2 className='subtit'>Que deseas hacer?</h2>
       <div className='panel'>
         <Link to="/administrador/cargar" className='hipervinc'> <button className='adminBtn'>Cargar nuevo producto</button> </Link>
         <Link to="/administrador/listar" className='hipervinc'> <button className='adminBtn'>Listar productos</button> </Link>
         <button className='adminBtn'>Editar productos</button>
-
       </div>
-
-    </>
+    </div>
   )
 }
 
