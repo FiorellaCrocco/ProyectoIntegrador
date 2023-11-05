c23d17eb337335c595f8cbd1b4694e7f615352a7
palomamendezc <palomamendezcasariego@gmail.com>
2023-11-04 21:53:33 -0400

    cambios caracteristicas

--- only-books-fe/src/Components/CaracteristicaLibro/CaracteristicaLibro.jsx
+++ only-books-fe/src/Components/CaracteristicaLibro/CaracteristicaLibro.jsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import LanguageIcon from '@mui/icons-material/Language';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import CakeIcon from '@mui/icons-material/Cake';
 import ExplicitIcon from '@mui/icons-material/Explicit';
-import './CaracteristicaLibro.css'
+import './CaracteristicaLibro.css';
+// import data from '../LibrosPaginados/libros';
 
-const CaracteristicaLibro = () => {
+const CaracteristicaLibro = ({id}) => {
+  const [bookData, setBookData] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the API endpoint
+    fetch(`http://localhost:5173/book/${id}`) //la URL actual API endpoint
+      .then(response => response.json())
+      .then(data => setBookData(data))
+      .catch(error => console.error('Error fetching data:', error));
+      console.log(bookData)
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
+
   return (
     <div className='divAmplio'> 
         <h4 className='caract'>Caracteristicas</h4>
-        <ul className='listaCarac'>
+        {bookData ? (
+          <ul className='listaCarac'>
             <li className='listaIcon'>
                 <LanguageIcon />
-                <p>Ingles</p>
+                <p>{bookData.language}</p>
             </li>
             
             <li className='listaIcon'>
                 <AutoStoriesIcon/>
-                <p>250 paginas</p>
+                <p>{bookData.pages} paginas</p>
             </li>
             
             <li className='listaIcon'>
                 <CakeIcon/>
-                <p>Mayores de 13</p>
+                <p>{bookData.ageRestriction}</p>
             </li>
 
             <li className='listaIcon'>
                 <ExplicitIcon/>
-                <p>Si</p>
+                <p>{bookData.explicit ? 'Si' : 'No'}</p>
             </li>
-
-
-        </ul>
-
-
+          </ul>
+        ) : (
+          <p>Cargando informacion...</p>
+        )}
     </div>
-  )
+  );
 }
 
-export default CaracteristicaLibro
+export default CaracteristicaLibro;
