b56395dc61752315e833a7879e0626cd119ed2d4
GarciaFernando <garciafc97@gmail.com>
2023-11-09 18:33:19 -0300

    Cambios de caracteristica de back

--- only-books-be/src/main/java/com/proyecto/onlybooks/controller/CaracteristicaController.java
+++ only-books-be/src/main/java/com/proyecto/onlybooks/controller/CaracteristicaController.java
@@ -1,13 +1,20 @@
 package com.proyecto.onlybooks.controller;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.proyecto.onlybooks.entity.Caracteristica;
 import com.proyecto.onlybooks.exceptions.ResourceNotFoundException;
 import com.proyecto.onlybooks.service.impl.CaracteristicaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
 import java.util.List;
+import java.util.UUID;
 
 @CrossOrigin(origins="http://localhost:5173")
 @RestController
@@ -18,6 +25,8 @@ public class CaracteristicaController {
     public CaracteristicaController(CaracteristicaService caracteristicaService) {
         this.caracteristicaService = caracteristicaService;
     }
+    @Autowired
+    private AmazonS3 s3;
 
     // En la url "/caracteristica/listar" retorno una lista de UserDTO
     @GetMapping("/listar")
@@ -35,6 +44,21 @@ public class CaracteristicaController {
     // En la url "/caracteristica/agregar" hacemos un POST para guardar el user
     @PostMapping("/agregar")
     public ResponseEntity<?> agregarCaracteristica(@RequestBody Caracteristica caracteristica) {
+        String base64Image = (caracteristica.getIcono() != null) ? caracteristica.getIcono() : "";
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+
+        try {
+            byte[] binaryData = Base64.getDecoder().decode(base64Image);
+
+            String key = caracteristica.getTitle() + "_" + UUID.randomUUID().toString() + ".jpg";
+
+            objectMetadata.setContentLength(binaryData.length);
+            s3.putObject("onlybooksbucket", key, new ByteArrayInputStream(binaryData), objectMetadata);
+            caracteristica.setIcono("https://onlybooksbucket.s3.amazonaws.com/" + key);
+        } catch (IllegalArgumentException | AmazonServiceException e) {
+            System.err.println("Error al procesar la imagen " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al agregar la caracteristica.");
+        }
         caracteristicaService.guardar(caracteristica);
         return ResponseEntity.status(HttpStatus.OK).body(caracteristica.getId());
     }
