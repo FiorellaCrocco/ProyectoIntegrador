af673a98db7dd62238ae9f28134e84ce5a7ed7e6
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-11-11 22:10:46 -0300

    Card Component

--- only-books-fe/src/Components/Card/Card.jsx
+++ only-books-fe/src/Components/Card/Card.jsx
@@ -3,13 +3,27 @@ import { Link } from 'react-router-dom';
 import './Card.css';
 
 const Card = ({ id, imgUrl, title, price }) => (
-    <li className="book" key={id}>
-        <Link to={`/detail/${id}`}>
-            <img src={imgUrl} alt={title} />
-        </Link>
-        <p className="title">{title}</p>
-        <p className="price">${price}</p>
-    </li>
+    <div className="body-cards">
+        <div className="card">
+            <div className="card-img">
+                <Link to={`/detail/${id}`}>
+                    <img src={imgUrl} alt={title} />
+                </Link>
+            </div>
+            <div className="card-title">
+                <h3>{title}</h3>
+            </div>
+            <div className="card-details">
+                <div className="price">
+                    <span>Precio</span>
+                    <p>${price}</p>
+                </div>
+            </div>
+            <button>Buy Now</button>
+        </div>
+    </div>
+
 );
 
 export default Card;
+
