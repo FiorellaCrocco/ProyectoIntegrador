bfd183d414093454a4bec257a3caf8091319dd40
Gu1ll3rm04r13l <arieldelfresno2690@gmail.com>
2023-11-30 17:45:38 -0300

    Comentarios de consoles

--- only-books-fe/src/Components/DetalleLibro/GenerateDates.jsx
+++ only-books-fe/src/Components/DetalleLibro/GenerateDates.jsx
@@ -1,38 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import DateObject from 'react-date-object';
 
-const GenerateDates = ({ reservas, obtenerFechas}) => {
- const [dates, setDates] = useState([]);
- console.log(reservas)
- 
+const GenerateDates = ({ reservas, obtenerFechas }) => {
+    const [dates, setDates] = useState([]);
+    // console.log(reservas)
 
- useEffect(() => {
-    let newDates = [];
-    reservas.map((reserva)=>{
-        let currentDate = new Date(reserva.startDate);
-        let finishDate = new Date(reserva.returnDate)
-        finishDate.setDate(finishDate.getDate())
-        currentDate.setDate(currentDate.getDate())
-        while (currentDate <= finishDate) {
-            const formattedDate = `${currentDate.getFullYear()}-${
-                currentDate.getMonth() + 1
-            }-${currentDate.getDate()}`;  
-        newDates.push(new DateObject(formattedDate));
-        currentDate.setDate(currentDate.getDate()+1);
-        }
-        setDates(newDates);
-    })
-    
-}, [reservas]);
 
-useEffect(()=>{
-     obtenerFechas(dates)
+    useEffect(() => {
+        let newDates = [];
+        reservas.map((reserva) => {
+            let currentDate = new Date(reserva.startDate);
+            let finishDate = new Date(reserva.returnDate)
+            finishDate.setDate(finishDate.getDate())
+            currentDate.setDate(currentDate.getDate())
+            while (currentDate <= finishDate) {
+                const formattedDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1
+                    }-${currentDate.getDate()}`;
+                newDates.push(new DateObject(formattedDate));
+                currentDate.setDate(currentDate.getDate() + 1);
+            }
+            setDates(newDates);
+        })
 
- },[dates])
+    }, [reservas]);
 
- return (
-    <></>
- );
+    useEffect(() => {
+        obtenerFechas(dates)
+
+    }, [dates])
+
+    return (
+        <></>
+    );
 };
 
 export default GenerateDates;
