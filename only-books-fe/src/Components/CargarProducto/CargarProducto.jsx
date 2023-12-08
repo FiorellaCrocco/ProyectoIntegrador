fa4c3ef4a894fe4737ea31e1ce27185d2b3706d2
FiorellaCrocco <fiorellacrocco@gmail.com>
2023-12-08 15:10:02 -0300

    fix alert cargar producto

--- only-books-fe/src/Components/CargarProducto/CargarProducto.jsx
+++ only-books-fe/src/Components/CargarProducto/CargarProducto.jsx
@@ -4,6 +4,8 @@ import { GlobalContext } from "../../Context/globalContext";
 import Swal from "sweetalert2";
 
 function CargarProducto() {
+  console.log("CargarProducto component mounted");
+
   const API_URL = import.meta.env.VITE_API_URL;
   const { fetchData, listaCategorias, listaCaracteristicas } =
     useContext(GlobalContext);
@@ -68,6 +70,7 @@ function CargarProducto() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("Submit button clicked");
     const listaLibros = await fetchData();
     const librosFormateados = listaLibros.map((book) =>
       book.title.toLowerCase()
@@ -97,132 +100,152 @@ function CargarProducto() {
         showLoaderOnConfirm: true,
         preConfirm: async () => {
           try {
+            document.querySelector('button[type="submit"]').disabled = true;
+
             const response = await fetch(url, settings);
             const data = await response.text();
             console.log(data);
+            if (response.ok) {
+              for (const category of selectedCategory) {
+                await fetchCategoria(data, category.id);
+              }
+              for (const caracteristica of selectedCaracteristica) {
+                await fetchCaracteristica(data, caracteristica.id);
+              }
+              // Mensaje de éxito
+              Swal.fire({
+                text: "Libro cargado con éxito",
+                icon: "success",
+              });
 
-            for (const category of selectedCategory) {
-              await fetchCategoria(data, category.id);
-            }
-            for (const caracteristica of selectedCaracteristica) {
-              await fetchCaracteristica(data, caracteristica.id);
-            }
-            // Mensaje de éxito
-            Swal.fire({
-              text: "Libro cargado con éxito",
-              icon: "success",
-            });
-            // Limpiar los campos del formulario
-            setFormData({
-              id: 0,
-              title: "",
-              author: "",
-              description: "",
-              isbn: "",
-              publication_year: "2023-11-03",
-              qualification: 0,
-              price: 0,
-              imagesBase64: [],
-            });
+              // Limpiar los campos del formulario
+              setFormData({
+                id: 0,
+                title: "",
+                author: "",
+                description: "",
+                isbn: "",
+                publication_year: "2023-11-03",
+                qualification: 0,
+                price: 0,
+                imagesBase64: [],
+              });
 
-            // Actualizar la lista de libros después de la carga exitosa
-            await fetchData();
+              // Actualizar la lista de libros después de la carga exitosa
+              await fetchData();
+            } else if (response.status == 413) {
+              Swal.fire({
+                position: "top-end",
+                text: "El tamaño que desea enviar es mayor al soportado",
+                icon: "error",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            } else {
+              Swal.fire({
+                text: "Error al agregar el libro.",
+                icon: "error",
+              });
+            }
           } catch (error) {
             console.log(error);
+          }finally {
+            // Habilitar el botón de envío después de completar la lógica
+            document.querySelector('button[type="submit"]').disabled = false;
           }
         },
         allowOutsideClick: () => !Swal.isLoading(),
-      })
+      });
+    }
+  };
+  async function fetchCategoria(bookId, categoriaId) {
+    const settings = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const url = `${API_URL}book/${bookId}/categoria/${categoriaId}`;
+    //   const url = `https://onlybooks.isanerd.club/api/book/${bookId}/categoria/${categoriaId}`;
+    try {
+      const response = await fetch(url, settings);
+      const data = await response.text();
+      console.log(data);
+    } catch (error) {
+      console.error("ERROR:", error);
     }
   }
-    async function fetchCategoria(bookId, categoriaId) {
-      const settings = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const url = `${API_URL}book/${bookId}/categoria/${categoriaId}`;
-      //   const url = `https://onlybooks.isanerd.club/api/book/${bookId}/categoria/${categoriaId}`;
-      try {
-        const response = await fetch(url, settings);
-        const data = await response.text();
-        console.log(data);
-      } catch (error) {
-        console.error("ERROR:", error);
-      }
+
+  async function fetchCaracteristica(bookId, caracteristicaId) {
+    const settings = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const url = `${API_URL}book/${bookId}/caracteristica/${caracteristicaId}`;
+    //    const url = `https://onlybooks.isanerd.club/api/book/${bookId}/caracteristica/${caracteristicaId}`;
+    try {
+      const response = await fetch(url, settings);
+      const data = await response.text();
+      console.log(data);
+    } catch (error) {
+      console.error("ERROR:", error);
     }
+  }
 
-    async function fetchCaracteristica(bookId, caracteristicaId) {
-      const settings = {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      };
-      const url = `${API_URL}book/${bookId}/caracteristica/${caracteristicaId}`;
-      //    const url = `https://onlybooks.isanerd.club/api/book/${bookId}/caracteristica/${caracteristicaId}`;
-      try {
-        const response = await fetch(url, settings);
-        const data = await response.text();
-        console.log(data);
-      } catch (error) {
-        console.error("ERROR:", error);
-      }
+  const renderCategoryOptions = () => {
+    const categorias = listaCategorias;
+    return categorias.map((category, index) => (
+      <div
+        key={index}
+        className={`${selectedCategory.includes(category) ? "selected" : ""}`}
+        onClick={() => handleCategoryChange(category)}
+      >
+        <label value={category}>{category.titulo}</label>
+      </div>
+    ));
+  };
+
+  const renderCaracteristicasOptions = () => {
+    const caracteristicas = listaCaracteristicas;
+    return caracteristicas.map((caracteristica, index) => (
+      <div
+        key={index}
+        className={`${
+          selectedCaracteristica.includes(caracteristica) ? "selected" : ""
+        }`}
+        onClick={() => handleCaracteristicaChange(caracteristica)}
+      >
+        <label value={caracteristica}>
+          {caracteristica.title.toUpperCase()}
+        </label>
+      </div>
+    ));
+  };
+
+  const handleCategoryChange = (category) => {
+    if (selectedCategory.includes(category)) {
+      setSelectedCategory(
+        selectedCategory.filter((c) => c.titulo !== category.titulo)
+      );
+    } else {
+      setSelectedCategory([...selectedCategory, category]);
     }
+  };
 
-    const renderCategoryOptions = () => {
-      const categorias = listaCategorias;
-      return categorias.map((category, index) => (
-        <div
-          key={index}
-          className={`${selectedCategory.includes(category) ? "selected" : ""}`}
-          onClick={() => handleCategoryChange(category)}
-        >
-          <label value={category}>{category.titulo}</label>
-        </div>
-      ));
-    };
-
-    const renderCaracteristicasOptions = () => {
-      const caracteristicas = listaCaracteristicas;
-      return caracteristicas.map((caracteristica, index) => (
-        <div
-          key={index}
-          className={`${
-            selectedCaracteristica.includes(caracteristica) ? "selected" : ""
-          }`}
-          onClick={() => handleCaracteristicaChange(caracteristica)}
-        >
-          <label value={caracteristica}>
-            {caracteristica.title.toUpperCase()}
-          </label>
-        </div>
-      ));
-    };
-
-    const handleCategoryChange = (category) => {
-      if (selectedCategory.includes(category)) {
-        setSelectedCategory(
-          selectedCategory.filter((c) => c.titulo !== category.titulo)
-        );
-      } else {
-        setSelectedCategory([...selectedCategory, category]);
-      }
-    };
-
-    const handleCaracteristicaChange = (caracteristica) => {
-      if (selectedCaracteristica.includes(caracteristica)) {
-        setSelectedCaracteristica(
-          selectedCaracteristica.filter((c) => c.title !== caracteristica.title)
-        );
-      } else {
-        setSelectedCaracteristica([...selectedCaracteristica, caracteristica]);
-      }
-    };
-    window.scrollTo(0,0)
+  const handleCaracteristicaChange = (caracteristica) => {
+    if (selectedCaracteristica.includes(caracteristica)) {
+      setSelectedCaracteristica(
+        selectedCaracteristica.filter((c) => c.title !== caracteristica.title)
+      );
+    } else {
+      setSelectedCaracteristica([...selectedCaracteristica, caracteristica]);
+    }
+  };
+ // window.scrollTo(0, 0);
 
     return (
       <div>
@@ -327,13 +350,13 @@ function CargarProducto() {
                 <>{renderCategoryOptions()}</>
               </div>
 
-              <div className="divSelect">
-                <label className="labels" htmlFor="caracteristicas">
-                  Características:
-                </label>
-                <>{renderCaracteristicasOptions()}</>
-              </div>
+            <div className="divSelect">
+              <label className="labels" htmlFor="caracteristicas">
+                Características:
+              </label>
+              <>{renderCaracteristicasOptions()}</>
             </div>
+          </div>
 
             <div className="div">
               <label className="labels" htmlFor="image">
